66c2f00d98e831150f5f8eddb23301b2d3180de1
feyaq13 <feyaq13@gmail.com>
2020-01-06 14:20:21 +0300

    правит структуру и позиционирование popup формы

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const $ = require('jquery');
 
 const container = $('.main-body');
-const popupForm = $('.main__add-job-seekers');
+const popupForm = $('.form-add-job-seekers');
 const buttonClassClose = $('.add-job-seekers__button-close')[0];
 
 $('.button-add').click(openDropdownForm);
@@ -10,17 +10,19 @@ $(buttonClassClose).click(closeDropdownForm);
 
 function openDropdownForm (e) {
   e.preventDefault();
-  popupForm.addClass('main__add-job-seekers--visible');
+  popupForm.addClass('form-add-job-seekers--visible');
   container.addClass('main-body--disabled');
 }
 
 function closeDropdownForm (e) {
 
+  console.log(e);
+
   if ($(e.target).is('.main-body')
     || $(e.target).is(buttonClassClose)
     || e.target.offsetParent === buttonClassClose) {
     e.preventDefault();
-    popupForm.removeClass('main__add-job-seekers--visible');
+    popupForm.removeClass('form-add-job-seekers--visible');
     container.removeClass('main-body--disabled');
   }
 }
